9d08688cf4d32338fc7946549d5cdbaf3e1f147b
dyestuff <axtemramazanov@mail.ru>
2026-06-27 03:43:37 +0300

    New object container
    Rewriting todoCollection with Map

--- src/todoCollection.ts
+++ src/todoCollection.ts
@@ -2,28 +2,28 @@ import { TodoItem } from "./todoItem.js"
 
 export class TodoCollection {
   private nextId: number = 1;
+  private itemMap = new Map<number, TodoItem>()
 
-  constructor (public userName: string,
-              public todoItems: TodoItem[] = []) {
-
+  constructor (public userName: string, todoItems: TodoItem[] = []) {
+	  todoItems.forEach(item => this.itemMap.set(item.id, item));
   }
-
-  addTodo(task: string): number {
+  
+   addTodo(task: string): number {
     while (this.getTodoById(this.nextId)) {
       this.nextId++
     }
-    this.todoItems.push(new TodoItem(this.nextId, task))
+    this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));  
     return this.nextId
 }
 
 getTodoById(id: number): TodoItem | undefined {
-    return this.todoItems.find(item => item.id === id)
+    return this.itemMap.get(id);
 }
 
 markComplete(id: number, complete: boolean) {
     const todoItem = this.getTodoById(id)
   if (todoItem) {
-    todoItem.complete = complete
-  }
-}
+    	todoItem.complete = complete
+  	}
+    }
 }
